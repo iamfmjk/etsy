7ae4289c90b8823cf95ef24d1ab808a0ee07ef76
Patrick Reagan <patrick.reagan@viget.com>
2009-05-22 22:19:42 -0400

    Convert test macro over to Shoulda w/ merge_block()

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,48 +7,43 @@ require 'throat_punch'
 require File.dirname(__FILE__) + '/../lib/etsy'
 
 class Test::Unit::TestCase
-  
+
   def self.read_fixture(method_name)
     file = File.dirname(__FILE__) + "/fixtures/#{method_name}.json"
     JSON.parse(File.read(file))['results']
   end
-  
+
   def read_fixture(method_name)
     self.class.read_fixture(method_name)
   end
-  
+
   def mock_request_cycle(options)
     response = Etsy::Response.new(stub())
-    
+
     data = read_fixture(options[:data])
     data = data.first if data.size == 1
-    
+
     response.stubs(:result).with().returns(data)
-    
+
     Etsy::Request.stubs(:get).with(options[:for]).returns(response)
-    
+
     response
   end
-  
+
   def self.when_populating(klass, options, &block)
     data = options[:from].is_a?(String) ? read_fixture(options[:from])[0] : options[:from].call
-    
-    class_eval do
-      define_method "setup_for_population" do
-        @object = klass.new(data)
-      end
+
+    context "with data populated for #{klass}" do
+      setup { @object = klass.new(data) }
+      merge_block(&block)
     end
-    
-    block.call
+
   end
 
   def self.value_for(method_name, options)
-    class_eval do
-      should "have a value for :#{method_name}" do
-        setup_for_population
-        @object.send(method_name).should == options[:is]
-      end
+    should "have a value for :#{method_name}" do
+      @object.send(method_name).should == options[:is]
     end
   end
-  
+
 end
