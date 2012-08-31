be294549160bb1eaf3aacc86dbc1ea5fc5bffaed
Roger Smith <rogsmith@gmail.com>
2012-08-30 23:01:36 -0400

    ability to create a favorite listing

--- lib/etsy/favorite_listing.rb
+++ lib/etsy/favorite_listing.rb
@@ -4,6 +4,13 @@ module Etsy
 
     attributes :user_id, :listing_state, :listing_id, :create_date
 
+    #Create a new favorite listing
+    #
+    def self.create(user listing, options = {})
+      options.merge!(:require_secure => true)
+      post("/users/#{user.id}/favorites/listings/#{listing.id}", options)
+    end
+
     #Find all listings favorited by a user
     #
     def self.find_all_by_user_id(user_id, options = {})
