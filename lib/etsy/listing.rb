de986350d05774bcff1859ae9d761b15b724dd7d
Roger Smith <rogsmith@gmail.com>
2012-09-01 22:25:14 -0400

    pass credentials

--- lib/etsy/listing.rb
+++ lib/etsy/listing.rb
@@ -158,6 +158,7 @@ module Etsy
     #Return a list of users who have favorited this listing
     #
     def admirers(options = {})
+      options = options.merge(:access_token => token, :access_secret => secret) if (token && secret)
       favorite_listings = FavoriteListing.find_all_listings_favored_by(id, options)
       user_ids  = favorite_listings.map {|f| f.user_id }.uniq
       (user_ids.size > 0) ? Array(Etsy::User.find(user_ids, options)) : []
