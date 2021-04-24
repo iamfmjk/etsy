05eb41f54bd8d700559d9efd51c0f48edc893165
Bui The Hien <buithehien1991@gmail.com>
2021-04-24 16:06:55 +0700

    Update taxonomy

--- lib/etsy/taxonomy.rb
+++ lib/etsy/taxonomy.rb
@@ -6,11 +6,11 @@ module Etsy
     attributes :level, :name, :parent, :parent_id, :path, :category_id, :children, :children_ids, :full_path_taxonomy_ids
 
     def self.get_buyer_taxonomy
-      get('/taxonomy/buyer/get')
+      get_all('/taxonomy/buyer/get')
     end
 
     def self.get_seller_taxonomy
-      get('/taxonomy/seller/get')
+      get_all('/taxonomy/seller/get')
     end
 
     def self.get_seller_taxonomy_version
