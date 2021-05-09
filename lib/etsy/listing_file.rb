142e1564c4592ff89209545942f109648d7d9387
Bui The Hien <buithehien1991@gmail.com>
2021-05-09 15:17:26 +0700

    Correct static method for ListingFile

--- lib/etsy/listing_file.rb
+++ lib/etsy/listing_file.rb
@@ -7,19 +7,19 @@ module Etsy
 
     attributes :rank, :filename, :filesize, :size_bytes, :filetype, :create_date
 
-    def find_files_by_listing(listing_id, options)
+    def self.find_files_by_listing(listing_id, options)
       get_all("/listings/#{listing_id}/files", options)
     end
 
-    def upload_listing_file(listing_id, options)
+    def self.upload_listing_file(listing_id, options)
       post("/listings/#{listing_id}/files", options)
     end
 
-    def find_listing_file(listing_id, listing_file_id, options)
+    def self.find_listing_file(listing_id, listing_file_id, options)
       get("/listings/#{listing_id}/files/#{listing_file_id}", options)
     end
 
-    def delete_listing_file(listing_id, listing_file_id, options)
+    def self.delete_listing_file(listing_id, listing_file_id, options)
       delete("/listings/#{listing_id}/files/#{listing_file_id}", options)
     end
   end
--- lib/etsy/version.rb
+++ lib/etsy/version.rb
@@ -1,3 +1,3 @@
 module Etsy
-  VERSION = "0.3.14"
+  VERSION = "0.3.14.1"
 end
