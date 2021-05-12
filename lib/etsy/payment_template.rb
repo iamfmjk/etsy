58203a1c3869a650aef7fe55ab2978864e171d93
Jace Bayless <jacebayless@gmail.com>
2021-05-12 21:56:22 +0000

    Updated URL for PaymentTemplates

--- lib/etsy/payment_template.rb
+++ lib/etsy/payment_template.rb
@@ -7,18 +7,18 @@ module Etsy
     attributes :paypal_email, :name, :first_line, :second_line, :city, :state
     attributes :zip, :country_id
 
-    def self.create(options = {})
+    def self.create(shop, options = {})
       options.merge!(:require_secure => true)
-      post("/payments/templates", options)
+      post("/shops/#{shop.id}/payment_templates", options)
     end
  
-    def self.find(id, credentials = {})
+    def self.find(shop, credentials = {})
       options = {
         :access_token => credentials[:access_token],
         :access_secret => credentials[:access_secret],
         :require_secure => true
       }
-      get("/payments/templates/#{id}", options)
+      get("/shops/#{shop.id}/payment_templates", options)
     end
 
     def self.find_by_user(user, credentials = {})
